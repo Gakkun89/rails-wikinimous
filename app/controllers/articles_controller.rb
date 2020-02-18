e5c6af03a4af3d1c4d13f0e0531449a08fa89f11
Mike Warren <mikeywarren89@gmail.com>
2020-02-18 14:10:01 +0900

    added private classes to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  private
+
+  def article_params
+    params.require(:article).permit(:content, :title)
+  end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
