4bb4fcc8e8c754d5745e2d7b7af9e10df53ecda6
Mike Warren <mikeywarren89@gmail.com>
2020-02-18 14:24:14 +0900

    completed methods in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,49 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @task.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:content, :title)
   end
+
   def set_article
     @article = Article.find(params[:id])
   end
